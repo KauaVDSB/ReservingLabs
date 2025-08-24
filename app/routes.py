c413eece4e06f035de166715c284cd55a6ac8f30
KakisCoding <kauavdsb.jobs@gmail.com>
2025-08-24 15:23:54 -0300

    feat(labs): Cria pagina para renderizar tabela com todos os laboratorios.

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for
 from flask_login import login_required, login_user, logout_user, current_user
 
 from app import app, db
-from app.models import User
+from app.models import User, Laboratorio
 from app.forms import UserForm, LabForm
 
 
@@ -12,20 +12,20 @@ def homepage():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/auth/login")
 def login():
     """Renderiza rota para login"""
     return render_template("auth/login.html")
 
 
-@app.route("/logout")
+@app.route("/auth/logout")
 def logout():
     """ Desloga usuário a sessão """
     logout_user()
     return redirect(url_for("homepage"))
 
 
-@app.route("/cadastro", methods=["GET", "POST"])
+@app.route("/auth/cadastro", methods=["GET", "POST"])
 def cadastro():
     """ Renderiza rota de cadastro """
     form = UserForm()
@@ -55,6 +55,13 @@ def labs_create():
                 f'<a href="{url_homepage}">Voltar</a>')
 
     return render_template("labs/create.html", form=form)
+
+
+@app.route("/labs/list", methods=["GET", "POST"])
+def labs_list():
+    labs = Laboratorio.query.all()
+
+    return render_template("labs/list.html", labs=labs)
 
 
 @app.route("/tornar-admin/<int:user_id>", methods=["GET", "POST"])
