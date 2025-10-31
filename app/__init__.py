ac922e054c4608aaed65053dfb6ab002453e3250
KauaVDSB <kauavdsb.jobs@gmail.com>
2025-10-31 19:37:33 -0300

    fix(senha): corrige encriptacao de senha do usuario

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,12 @@ import os  #mexer no sistema operacional (ler arquivos)
 from flask import Flask  #função principal
 from flask_sqlalchemy import SQLAlchemy  #CRUD
 from flask_migrate import Migrate  #modelo-->bd
-# from flask_bcrypt import Bcrypt  #criptografia//segurança
+from flask_bcrypt import Bcrypt  #criptografia//segurança
 from flask_login import LoginManager  #autodescritivo
 from apscheduler.schedulers.background import BackgroundScheduler  #agendador de tarefas
 import atexit  #encerrar tarefas
+
+from supabase import create_client
 
 from dotenv import load_dotenv
 
@@ -25,11 +27,16 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # Inicializa ferramentas para manipulação do banco de dados
 db = SQLAlchemy(app)  #não é pasta, e sim a variavel app
 migrate = Migrate(app, db)  #inicializa variavel
-# bcrypt = Bcrypt(app)
+bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = (
     "login"  # Redireciona usuários para esta rota, caso estejam deslogados.
 )
+
+# Configurações SUPABASE
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 from app.models import User, Laboratorio, Solicitacao
 
